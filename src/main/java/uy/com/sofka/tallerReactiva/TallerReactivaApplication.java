415b705a8f8fc6b5534b18045c15daaf81443bb6
francheskito <franccesco.giordano11@gmail.com>
2022-03-17 15:54:30 -0300

    añadi (exclude = {DataSourceAutoConfiguration.class }) al Main por un error que tiraba

--- src/main/java/uy/com/sofka/tallerReactiva/TallerReactivaApplication.java
+++ src/main/java/uy/com/sofka/tallerReactiva/TallerReactivaApplication.java
@@ -2,8 +2,9 @@ package uy.com.sofka.tallerReactiva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TallerReactivaApplication {
 
     public static void main(String[] args) {
